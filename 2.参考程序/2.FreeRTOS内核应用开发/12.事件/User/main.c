58fda0e12e50be5dba9bd8b2e875caf6a0d43a16
hazy1k <a249007421@gmail.com>
2025-03-06 20:30:25 +0800

    code

---
+++ 2.参考程序/2.FreeRTOS内核应用开发/12.事件/User/main.c
@@ -0,0 +1,140 @@
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "event_groups.h"
+/* 其他头文件 */
+#include "uart.h"
+#include "led.h"
+#include "key.h"
+
+/* 任务句柄 */
+static TaskHandle_t AppTaskCreate_Handle = NULL; // 创建任务句柄
+
+static TaskHandle_t LED_Task_Handle = NULL; // LED任务句柄
+static TaskHandle_t KEY_Task_Handle = NULL; // KEY任务句柄
+
+/* 内核对象句柄 */
+static EventGroupHandle_t Event_Handle = NULL; // 事件组句柄
+/* 全局变量声明 */
+/* 宏定义 */
+#define KEY1_EVENT (0x01 << 0) // 设置事件掩码的位0
+#define KEY2_EVENT (0x01 << 1) // 设置事件掩码的位1
+
+/* 任务函数声明 */
+static void AppTaskCreate(void); // 创建任务函数
+
+static void LED_Task(void* pvParameters); // LED任务函数
+static void KEY_Task(void* pvParameters); // KEY任务函数
+
+static void BSP_Init(void); // 板级初始化函数
+
+// 主函数启动流程
+/*
+    1.BSP初始化
+    2.创建APP任务
+    3.启动FreeRTOS,开启调度
+*/
+int main(void)
+{
+    BaseType_t xReturn = pdPASS; // 定义一个创建信息返回值，默认为pdPASS
+    BSP_Init(); // 板级初始化
+    printf("FreeRTOS事件标志位实验\r\n");
+    // 创建AppTaskCreate任务   
+    xReturn = xTaskCreate((TaskFunction_t )AppTaskCreate,
+                        (const char*)"AppTaskCreate",
+                        (uint16_t)512,  
+                        (void*)NULL,
+                        (UBaseType_t)1, 
+                        (TaskHandle_t*)&AppTaskCreate_Handle); 
+    if(xReturn == pdPASS)
+    {
+        vTaskStartScheduler(); // 启动FreeRTOS
+    }
+    else
+    {
+        return -1;
+    }
+    while(1);
+}
+
+// 为了方便管理，所有任务创建函数都在AppTaskCreate中实现
+static void AppTaskCreate(void)
+{
+  BaseType_t xReturn = pdPASS;
+  taskENTER_CRITICAL(); // 进入临界区
+
+  Event_Handle = xEventGroupCreate(); // 创建事件组
+  if(Event_Handle != NULL)
+  {
+    printf("事件组创建成功\r\n");
+  }
+  xReturn = xTaskCreate((TaskFunction_t )LED_Task,
+                        (const char*)"LED_Task",
+                        (uint16_t)512,  
+                        (void*)NULL,
+                        (UBaseType_t)2, 
+                        (TaskHandle_t*)&LED_Task_Handle); // 创建LED任务
+  if(xReturn == pdPASS)
+  {
+    printf("LED任务创建成功\r\n");
+  }
+  xReturn = xTaskCreate((TaskFunction_t )KEY_Task,
+                        (const char*)"KEY_Task",
+                        (uint16_t)512,  
+                        (void*)NULL,
+                        (UBaseType_t)2, 
+                        (TaskHandle_t*)&KEY_Task_Handle); // 创建KEY任务
+  if(xReturn == pdPASS)
+  {
+    printf("KEY任务创建成功\r\n");
+  }
+
+  vTaskDelete(AppTaskCreate_Handle);
+  taskEXIT_CRITICAL(); // 退出临界区
+}
+
+// LED任务函数
+static void LED_Task(void* pvParameters)
+{
+  EventBits_t r_event;
+  while(1)
+  {
+    r_event = xEventGroupWaitBits(Event_Handle, KEY1_EVENT | KEY2_EVENT, pdTRUE, pdTRUE, portMAX_DELAY);
+    if(r_event & (KEY1_EVENT | KEY2_EVENT) == (KEY1_EVENT | KEY2_EVENT))
+    {
+      printf("KEY1和KEY2同时按下\r\n");
+      LED1_TOGGLE();
+    }
+    else{
+      printf("事件错误\r\n");
+    }
+  }
+}
+
+// KEY任务函数
+static void KEY_Task(void* pvParameters)
+{
+  while(1)
+  {
+    if(Key_Scan(KEY1_GPIO_PORT, KEY1_GPIO_PIN) == KEY_ON)
+    {
+      printf("KEY1按下\r\n");
+      xEventGroupSetBits(Event_Handle, KEY1_EVENT);
+    }
+    if(Key_Scan(KEY2_GPIO_PORT, KEY2_GPIO_PIN) == KEY_ON)
+    {
+      printf("KEY2按下\r\n");
+      xEventGroupSetBits(Event_Handle, KEY2_EVENT);
+    }
+    vTaskDelay(20);
+  }
+}
+
+// 板级初始化函数
+static void BSP_Init(void)
+{
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4); // 设置中断优先级分组4
+    LED_Init();
+    USART_Config();
+    Key_GPIO_Config();
+}
